2455f5a6eb2c34b196ee373697991c39bd5f06c9
Desector123456 <coronadoc899@gmail.com>
2023-12-28 04:40:26 -0300

    Final Seccion 22

---
+++ src/calendar/pages/CalendarPage.jsx
@@ -0,0 +1,75 @@
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { CalendarEvent, CalendarModal, FabAddNew, Navbar } from '../';
+
+import { getMessagesEs, localizer } from '../../helpers';
+import { useState } from 'react';
+import { useUiStore } from '../../hooks/useUiStore';
+import { useCalendarStore } from '../../hooks';
+import { FabDelete } from '../components/FabDelete';
+
+export const CalendarPage = () => {
+  const { openDateModal } = useUiStore();
+
+  const { events, setActiveEvent } = useCalendarStore();
+
+  const [lastView, setLastView] = useState(
+    localStorage.getItem('lastView') || 'week'
+  );
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+    };
+
+    return {
+      style,
+    };
+  };
+
+  const onDoubleClick = (event) => {
+    // console.log({ dobleClick: event });
+    openDateModal();
+  };
+  const onSelect = (event) => {
+    setActiveEvent(event);
+  };
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastView', event);
+    setLastView(event);
+  };
+
+  return (
+    <>
+      <Navbar />
+
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        defaultView={lastView}
+        events={events}
+        startAccessor='start'
+        endAccessor='end'
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessagesEs()}
+        eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent,
+        }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
+      />
+
+      <CalendarModal />
+
+      <FabAddNew />
+
+      <FabDelete />
+    </>
+  );
+};
